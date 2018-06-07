4b9dcbe7323c6e9e9296622924a9cfe473cefab0
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2018-06-06 23:30:26 -0300

    test: Cover all cases for Configurations
    This patch covers the missing endpoints for Configurations resource.

--- tests/integration/test_client.py
+++ tests/integration/test_client.py
@@ -18,6 +18,7 @@ class TestConfiguration(object):
             configuration = client.create_configuration(
                 'Dummy configuration', 'www.example.com', 'ww2.example.com',
                 cname=['www.example-cname.com'], delivery_protocol='http')
+
         assert isinstance(configuration, Configuration)
         assert configuration.active is True
         assert configuration.id
@@ -28,3 +29,56 @@ class TestConfiguration(object):
         assert configuration.cname_access_only is False
         assert configuration.name == 'Dummy configuration'
         assert configuration.domain_name
+
+    def test_get_configuration(self):
+        client = Azion(token)
+        recorder = betamax.Betamax(client.session)
+
+        with recorder.use_cassette('Configuration_get'):
+            configuration = client.get_configuration(1528252734)
+
+        assert isinstance(configuration, Configuration)
+
+    def test_update_configuration(self):
+        client = Azion(token)
+        recorder = betamax.Betamax(client.session)
+
+        with recorder.use_cassette('Configuration_partial_update'):
+            configuration = client.partial_update_configuration(
+                1528252734, delivery_protocol='http,https')
+
+        assert isinstance(configuration, Configuration)
+        assert configuration.delivery_protocol == 'http,https'
+
+    def test_replace_configuration(self):
+        client = Azion(token)
+        recorder = betamax.Betamax(client.session)
+
+        with recorder.use_cassette('Configuration_replace'):
+            configuration = client.replace_configuration(
+                1528252734,
+                name='Dummy configuration',
+                delivery_protocol='http,https')
+
+        assert isinstance(configuration, Configuration)
+        assert configuration.delivery_protocol == 'http,https'
+
+    def test_list_configurations(self):
+        client = Azion(token)
+        recorder = betamax.Betamax(client.session)
+
+        with recorder.use_cassette('Configuration_list'):
+            configurations = client.list_configurations()
+
+        assert configurations
+        assert isinstance(configurations, list)
+        assert all(isinstance(configuration, Configuration)
+                   for configuration in configurations)
+
+    def test_delete_configuration(self):
+        client = Azion(token)
+        recorder = betamax.Betamax(client.session)
+
+        with recorder.use_cassette('Configuration_delete'):
+            assert client.delete_configuration(1528252734)
+
